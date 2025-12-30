64d234caaa0ceee03e4f021b5ca456a6bb9c8c3e
Kate Botten <katerjbotten@gmail.com>
2025-12-30 05:30:50 -0400

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,9 @@ module katestrophic.owc {
     requires javafx.controls;
     requires javafx.fxml;
 
+    // allow JavaFX (FXML) access to modules
+    opens katestrophic.owc to javafx.fxml;
+
+    // export the main package so the application can start
+    exports katestrophic.owc;
 }
