3b52a4405a5fd21bd837e534623e40c9f6636ade
Kate Botten <katerjbotten@gmail.com>
2025-12-30 05:39:06 -0400

    Add logic to Launcher.java

--- src/main/java/katestrophic/owc/Launcher.java
+++ src/main/java/katestrophic/owc/Launcher.java
@@ -1,4 +1,8 @@
 package katestrophic.owc;
 
+/// @purpose launch to the JavaFX Application entry point
 public class Launcher {
+    static void main (String[] args){
+        OWCApplication.launch(OWCApplication.class, args);
+    }
 }
