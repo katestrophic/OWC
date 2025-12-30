695441a8b2e36d5374697462b142b44b61b9d513
Kate Botten <katerjbotten@gmail.com>
2025-12-30 05:48:45 -0400

    Add logic to OWCApplication.java
    use placeholders to test UI function

--- src/main/java/katestrophic/owc/OWCApplication.java
+++ src/main/java/katestrophic/owc/OWCApplication.java
@@ -1,4 +1,24 @@
 package katestrophic.owc;
 
-public class OWCApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/// @purpose JavaFX remains separate from the Launcher
+public class OWCApplication extends Application {
+
+    @Override
+    public void start (Stage stage) throws IOException {
+        var root = new StackPane(new Label("OWC Dashboard"));
+        var scene = new Scene(root);
+
+        stage.setTitle("Ocean Workspace Companion");
+        stage.setScene(scene);
+
+        stage.show();
+    }
 }
